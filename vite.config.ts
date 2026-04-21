ec35a9167bd933a929f575e4e0bd2ea083c615a3
Gilmar46 <gilmarcoutobrito@gmail.com>
2026-04-21 15:20:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
 
     // 🔥 ESSA LINHA É O QUE FALTAVA
-    base: '/SEU-REPOSITORIO/',
+    base: '/APP-Eden/',
 
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
